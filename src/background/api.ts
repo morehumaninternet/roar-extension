971dc4f531ed69ed17371328d47250fd47f20496
Shachar Langer <shachar.langer@gmail.com>
2020-11-02 17:38:56 +0100

    Post status with screenshots

--- src/background/api.ts
+++ src/background/api.ts
@@ -1,13 +1,21 @@
 export const postTweet = async (toBeTweeted: ToBeTweeted, dispatchBackgroundActions: DispatchBackgroundActions) => {
+  const tweetData = new FormData()
+
   const status = toBeTweeted.feedbackState.editorState.getCurrentContent().getPlainText('\u0001')
+  tweetData.append('status', status)
+
+  // Adding all the screenshot files under the same form key - 'screenshots'.
+  // If there are multiple screenshots, 'screenshots' will be an array of file objects.
+  // If there's only one file, 'screenshots' will be a single file object.
+  toBeTweeted.feedbackState.screenshots.map((screenshot, index) => {
+    tweetData.append(`screenshots`, screenshot.blob, `screenshot name ${index}`)
+  })
+
   try {
     const res = await fetch(`${window.roarServerUrl}/v1/feedback`, {
       method: 'POST',
       credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ status })
+      body: tweetData
     })
     if (res.status !== 201) {
       return dispatchBackgroundActions.postTweetFailure(await res.text())
