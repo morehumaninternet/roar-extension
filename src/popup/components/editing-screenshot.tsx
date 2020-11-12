8eef554ba4be9a2e7af0543d1d9141946db662e0
Shachar Langer <shachar.langer@gmail.com>
2020-11-12 15:34:54 +0100

    Add react-sketch

--- src/popup/components/editing-screenshot.tsx
+++ src/popup/components/editing-screenshot.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
+import * as Sketch from 'react-sketch'
 
-export function EditingScreenshot({ color, blob }: Pick<EditingScreenshotState, 'blob' | 'color'>) {
+export function EditingScreenshot({ color, blob }: Pick<EditingScreenshotState, 'blob' | 'color'>): JSX.Element {
   const imageUrl = URL.createObjectURL(blob)
 
   return (
@@ -8,6 +9,7 @@ export function EditingScreenshot({ color, blob }: Pick<EditingScreenshotState, 
       EDITING
       <img className="screenshot-thumbnail" src={imageUrl} />
       <p>Color {color}</p>
+      <Sketch.SketchField width="1024px" height="768px" tool={Sketch.Tools.Pencil} lineColor="black" lineWidth={3} />
     </div>
   )
 }
