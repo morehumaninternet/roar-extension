2eabb897f4fd5d94268526b6232293919f1f5543
Hank Liu <hanke.liu@gmail.com>
2020-11-23 11:32:16 -0500

    add roar logo and footer text  for extension twitter login page, without style

--- src/popup/views/not-authed.tsx
+++ src/popup/views/not-authed.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react'
 
 export function NotAuthed({ signInWithTwitter }: Pick<Dispatchers<UserAction>, 'signInWithTwitter'>): JSX.Element {
-  return <button onClick={signInWithTwitter}>Sign in with twitter</button>
+  return (
+    <div className="not-authed">
+      <img src="../../../img/roar_128.png" width="128px" height="128px" />
+      <button onClick={signInWithTwitter}>
+        <img src="../../../img/twitter_logo_blue.png" width="32px" height="32px" />
+        Log in with twitter
+      </button>
+      <p>
+        Built with ❤️ by the team at <a href="<button onClick={signInWithTwitter}>Sign in with twitter</button>">morehumaninternet.org</a>
+      </p>
+    </div>
+  )
 }
