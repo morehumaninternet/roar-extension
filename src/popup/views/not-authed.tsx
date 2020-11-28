c23a1aa0f4836038c86fc3f1869e91c63a2f9887
Hank Liu <hanke.liu@gmail.com>
2020-11-27 19:57:43 -0500

    update img src path

--- src/popup/views/not-authed.tsx
+++ src/popup/views/not-authed.tsx
@@ -3,9 +3,9 @@ import * as React from 'react'
 export function NotAuthed({ signInWithTwitter }: Pick<Dispatchers<UserAction>, 'signInWithTwitter'>): JSX.Element {
   return (
     <div className="not-authed">
-      <img src="../../../img/roar_128.png" />
+      <img src="/img/roar_128.png" />
       <button onClick={signInWithTwitter}>
-        <img src="../../../img/twitter_logo_blue.svg" />
+        <img src="/img/twitter_logo_blue.svg" />
         Log in with Twitter
       </button>
       <p>
