a2d5ad8133c46b8a3025894b89b5d53417f73a75
Angel Ortiz <Bakedfood@users.noreply.github.com>
2020-11-29 20:02:16 -0500

    Adds Styling to Tweeting page

--- src/popup/components/tweeting.tsx
+++ src/popup/components/tweeting.tsx
@@ -1,5 +1,15 @@
 import * as React from 'react'
 
 export function Tweeting({ at }: { at: string }): JSX.Element {
-  return <p className="tweet-in-progress">Tweeting your feedback to {at}</p>
+  return (
+    <>
+      <>
+        <div className="tweeting-box">
+          <img className="tweeting-roar" src="../../../img/roar_128.png"></img>
+        </div>
+        <p className="tweet-in-progress">Tweeting your feedback to {at}</p>
+      </>
+      <div className="tweeting-spinner"></div>
+    </>
+  )
 }
