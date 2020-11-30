ddb575fdf4880a9efef29eb1d29206c70339012a
William Weiss <will.weiss1230@gmail.com>
2020-11-30 17:48:59 -0500

    Sign in page style (#92)
    * add twitter logo png file for log in button
    
    * add roar logo and footer text  for extension twitter login page, without style
    
    * inside not-authed div, add .login div to wrap login icon, button and footer
    
    * add styles for login page. work in progress
    
    * set up min height and min width for not-authed div. login element displayed evenly on popup body height
    
    * add twitter-blue
    
    * consolidate parent and child selectors with nesting styles
    
    * remove button border and outline. Change button background to white and font to bold
    
    * swap twitter logo with svg file and remove the png file from image folder
    
    * moved img inline style into scss file. Adjusted twitter login logo size
    
    * change sign in with twitter to log in with twitter
    
    * correct typo
    
    * refactor testing syntax for Log in button to match innerHTML content and skip twitter logo
    
    * add twitter logo png file for log in button
    
    * add roar logo and footer text  for extension twitter login page, without style
    
    * resolve merge conflicts. accept both changes
    
    * resolve merge conflicts. accept both changes
    
    * consolidate parent and child selectors with nesting styles
    
    * remove button border and outline. Change button background to white and font to bold
    
    * swap twitter logo with svg file and remove the png file from image folder
    
    * resolved merge confilicts. accept current changes.
    
    * refactor testing syntax for Log in button to match innerHTML content and skip twitter logo
    
    * remove duplication describe block for log in button testing
    
    * resolved merge conflicts. accept current change and manually.
    
    * update url https://morehumaninternet.org
    
    * update img src path
    
    * correct wrong button element mixed with hyperlink tag in log in page
    
    * convert twitter svg logo to react component. removed old twitter logo file in image folder
    
    * remove png roar logo with svg file
    
    Co-authored-by: Hank Liu <hanke.liu@gmail.com>

--- src/tests/integration/steps/mount-popup.ts
+++ src/tests/integration/steps/mount-popup.ts
@@ -27,7 +27,7 @@ export function mountPopup(mocks: Mocks, opts: MountPopupOpts = {}): void {
       it('mounts the app with a button to sign in with twitter', () => {
         const appContainer = mocks.popupWindow.document.getElementById('app-container')!
         const signInWithTwitter = appContainer.querySelector('button')!
-        expect(signInWithTwitter).to.have.property('innerHTML', 'Sign in with twitter')
+        expect(signInWithTwitter.innerHTML).to.match(/.+(Log in with Twitter)$/)
       })
     }
 
