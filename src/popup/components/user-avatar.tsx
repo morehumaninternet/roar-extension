77313939d73a4889a6dbd3522527f5f729e51bff
Hank Liu <hanke.liu@gmail.com>
2020-12-01 16:16:38 -0500

    add hover dropdown div for profie-img. Dropdown content can not stay when mouse hover away from img. work in progress

--- src/popup/components/user-avatar.tsx
+++ src/popup/components/user-avatar.tsx
@@ -1,5 +1,12 @@
 import * as React from 'react'
 
 export function UserAvatar({ src }) {
-  return <img className="profile-img" src={src} />
+  return (
+    <div className="user-avatar">
+      <img className="profile-img" src={src} />
+      <div className="dropdown">
+        <p className="dropdown-content">Logout</p>
+      </div>
+    </div>
+  )
 }
