9c189437b09d3981a89dc9987e892fc0a8fd8317
Angel Ortiz <Bakedfood@users.noreply.github.com>
2020-12-02 16:18:48 -0500

    Fixes as requested

--- src/popup/components/tweeting.tsx
+++ src/popup/components/tweeting.tsx
@@ -2,14 +2,12 @@ import * as React from 'react'
 
 export function Tweeting({ at }: { at: string }): JSX.Element {
   return (
-    <>
-      <>
-        <div className="tweeting-box">
-          <img className="tweeting-roar" src="../../../img/roar_128.png"></img>
-        </div>
-        <p className="tweet-in-progress">Tweeting your feedback to {at}</p>
-      </>
+    <div className="tweeting">
+      <div className="tweeting-logo-container">
+        <img className="tweeting-logo" src="/img/roar_128.png"></img>
+      </div>
+      <div className="tweet-in-progress">Tweeting your feedback to {at}</div>
       <div className="tweeting-spinner"></div>
-    </>
+    </div>
   )
 }
