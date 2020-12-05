3884e33c0b6bcbcfea89438dc51537b47ad9c4a8
Hank Liu <hanke.liu@gmail.com>
2020-12-04 21:28:35 -0500

    change code pattern on character-countdown to add the remaining percentage count down circle. add style for the remaining count down circle in action-bar.scss.

--- src/popup/components/character-countdown.tsx
+++ src/popup/components/character-countdown.tsx
@@ -1,48 +1,49 @@
 import * as React from 'react'
-
 export const CharacterCountdown = ({ characterLimit }: { characterLimit: CharacterLimit }): JSX.Element => {
   const { remaining, percentageCompleted } = characterLimit
-
   // We draw a progress circle only if the user didn't cross the limit of characters
   const drawCircle = remaining >= 0
-
   // As the user gets closer to the limit of characters, we will draw a warning circle
   const drawWarningCircle = remaining <= 20
-
   // We draw the countdown only when the user gets closer to the limit.
   // After the user crossed the limit, we draw the number of excess characters
   const drawCountdown = remaining <= 20
   const drawWarningCountDown = remaining >= 0
-
-  // we draw a remaining progress circle
-  const drawRemainingCircle = remaining >= 1
-
+  const strokeDasharray = 56.5487 // stole from Twitter 🤷‍♀️
+  const strokeDashoffset = strokeDasharray - strokeDasharray * (percentageCompleted / 100)
+  const radius = 9
+  const strokeWidth = 2
+  const diameter = 2 * radius + strokeWidth
   return (
-    <svg viewBox="0 0 36 36" className="circular-chart">
-      {drawCircle && (
-        <path
-          className={drawWarningCircle ? 'circle__warning' : 'circle'}
-          strokeDasharray={`${percentageCompleted}, 100`}
-          d="M18 2.0845
-        a 15.9155 15.9155 0 0 1 0 31.831
-        a 15.9155 15.9155 0 0 1 0 -31.831"
-        />
-      )}
-      {drawRemainingCircle && (
-        <path
-          className={drawWarningCircle ? 'circle__warning' : 'circle_remaining'}
-          strokeDashoffset={`${100 - percentageCompleted}, 100`}
-          d="M18 2.0845
-        a 15.9155 15.9155 0 0 1 0 31.831
-        a 15.9155 15.9155 0 0 1 0 -31.831"
-        />
-      )}
-
-      {drawCountdown && (
-        <text x="18" y="22.35" className={drawWarningCountDown ? 'inner-text__warning' : 'inner-text'}>
-          {Math.abs(remaining)}
-        </text>
-      )}
-    </svg>
+    <div
+      className="character-countdown"
+      role="progressbar"
+      aria-valuemax={100}
+      aria-valuemin={0}
+      aria-valuenow={Math.max(100, Math.floor(percentageCompleted))}
+    >
+      <svg viewBox={`0 0 ${diameter} ${diameter}`} className={`circular-chart ${drawWarningCircle ? 'warning' : ''}`}>
+        {drawCircle && (
+          <>
+            <circle cx="50%" cy="50%" stroke-width={strokeWidth} r={radius}></circle>
+            <circle
+              className="progress"
+              cx="50%"
+              cy="50%"
+              stroke-width={strokeWidth}
+              r={radius}
+              transform-origin="center"
+              stroke-linecap="round"
+              style={{ strokeDashoffset, strokeDasharray, transform: 'rotate(-90deg)' }}
+            ></circle>
+          </>
+        )}
+        {drawCountdown && (
+          <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" className={drawWarningCountDown ? 'inner-text__warning' : 'inner-text'}>
+            {Math.abs(remaining)}
+          </text>
+        )}
+      </svg>
+    </div>
   )
 }
