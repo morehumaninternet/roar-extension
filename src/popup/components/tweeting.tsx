5efeb79a5d7f122154925d130084a881212d2306
Will Weiss <will.weiss1230@gmail.com>
2020-12-15 10:49:30 -0500

    use new logo

--- src/popup/components/tweeting.tsx
+++ src/popup/components/tweeting.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
+import { RoarLogo } from './roar-logo'
 
 export function Tweeting({ at }: { at: string }): JSX.Element {
   return (
     <div className="tweeting">
       <div className="tweeting-logo-container">
-        <img className="tweeting-logo" src="/img/roar_128.png"></img>
+        <RoarLogo />
       </div>
       <div className="tweet-in-progress">Tweeting your feedback to {at}</div>
       <div className="tweeting-spinner"></div>
