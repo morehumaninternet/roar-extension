a4be9ea768d9f21cc78716ecd8707c5ad4559ec0
Will Weiss <will.weiss1230@gmail.com>
2021-01-14 08:03:35 -0500

    adds separator between buttons that update feedback and those that don't

--- src/popup/components/action-bar.tsx
+++ src/popup/components/action-bar.tsx
@@ -17,6 +17,14 @@ type ActionBarProps = {
   isTweeting: boolean
 }
 
+const Separator = (): JSX.Element => (
+  <div className="separator">
+    <svg viewBox="0 0 2 2">
+      <circle cx="1" cy="1" r="1" />
+    </svg>
+  </div>
+)
+
 export const ActionBar = ({
   clickPost,
   togglePickingEmoji,
@@ -38,6 +46,7 @@ export const ActionBar = ({
         <ActionButton kind="TakeScreenshot" onClick={clickTakeScreenshot} disabled={addImageDisabled} />
         <ActionButton kind="AddImage" onClick={() => imageRef.current!.click()} disabled={addImageDisabled} />
         <ActionButton kind="AddEmoji" onClick={togglePickingEmoji} additionalClassNames={pickingEmoji ? 'on' : 'off'} />
+        <Separator />
         <ActionButton kind={darkModeOn ? 'DarkMode' : 'LightMode'} onClick={toggleDarkMode} />
         <a title="Get Help" className="svg-btn Help" role="button" href="mailto:support@morehumaninternet.org?subject=Roar! extension">
           {svgs.Help}
@@ -53,7 +62,7 @@ export const ActionBar = ({
         }}
       />
       <CharacterCountdown characterLimit={characterLimit} />
-      <button className="post-btn" onClick={clickPost} disabled={isTweeting || postTweetDisabled}>
+      <button className="post-btn" onClick={clickPost} disabled={isTweeting || postTweetDisabled} title="Post Feedback">
         {isTweeting ? 'Posting...' : 'Post'}
       </button>
     </div>
